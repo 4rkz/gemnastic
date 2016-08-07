a4c817c5bc223de7f9c7ee596309e2759d4f96d5
Arek <arek.zd@gmail.com>
2016-08-07 15:58:50 +0200

    generated redactor for --devise

--- gemnastic/config/routes.rb
+++ gemnastic/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount RedactorRails::Engine => '/redactor_rails'
   resources :posts
   devise_for :users, controllers: { omniauth_callbacks: "acme/omniauth_callbacks", registrations: "acme/registrations" }
 
